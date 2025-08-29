fb477e4d609a5492a9e8dcc8f6e722ba1951b891
MARS77662 <d477188@gmail.com>
2025-08-30 04:28:52 +0800

    Add files via upload

--- app/xiuxian/page.jsx
+++ app/xiuxian/page.jsx
@@ -1,12 +1,10 @@
-import dynamic from "next/dynamic";
-
-// 不要快取、不要預先產生
+// 不要快取、不要預產
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
-// 用 next/dynamic 關掉 SSR
-const Client = dynamic(() => import("./ClientPage"), { ssr: false });
+// 直接引入 client component（不要用 next/dynamic）
+import ClientPage from "./ClientPage";
 
 export default function Page() {
-  return <Client />;
+  return <ClientPage />;
 }
